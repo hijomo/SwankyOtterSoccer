797898f067c050485701a514c0bcac6982487ad2
hijomo <hijomo@gmail.com>
2016-12-11 19:34:09 -0700

    first commit

---
+++ Assets/Scripts/KillParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillParticle : MonoBehaviour {
+
+
+  float killTime;
+	// Use this for initialization
+	void Start () {
+    killTime = Time.time+4.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Time.time > killTime)
+      Destroy(this.gameObject);
+		
+	}
+}
---
+++ Assets/Scripts/goalScipt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalScipt : MonoBehaviour {
+
+
+  public GameObject otterInstance, particalInstance;
+  AudioSource au;
+	// Use this for initialization
+	void Start () {
+    au = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject == GameObject.FindGameObjectWithTag("Otter"))
+    {
+      Instantiate(particalInstance, other.transform.position,other.transform.rotation);
+      Instantiate(otterInstance, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
+      au.Play();
+      Destroy(other.gameObject);
+    }
+    
+  }
+
+}
